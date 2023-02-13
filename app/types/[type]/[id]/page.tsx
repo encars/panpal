30965373738f7cb27c81e0b7a4ee77ad4857e053
Enric Carstens <enric.carstens@gmail.com>
2023-02-13 14:54:05 +0100

    added details

--- app/types/[type]/[id]/page.tsx
+++ app/types/[type]/[id]/page.tsx
@@ -1,16 +1,45 @@
 import React from "react";
+import Image from "next/image";
 
 interface Props {
     params: {
-
+        id: number;
     };
 }
 
-const page = (props: Props) => {
-    console.log(props.params);
+const getRecipeDetails = async (id: number) => {
+    const res = await fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
+    return res.json();
+};
+
+const page = async (props: Props) => {
+    const recipeDetails = await getRecipeDetails(props.params.id);
+    const details = recipeDetails.meals[0];
+    const ingredients = Object.keys(details)
+        .filter((key) => key.indexOf("Ingredient") > 0)
+        .map((ingKey) => details[ingKey])
+        .filter(Boolean);
+
     return (
-        <div>
-            page
+        <div className="grid grid-cols-1 md:grid-cols-2">
+            <div>
+                <Image className="w-full" src={details.strMealThumb} alt="Recipe Image" width={500} height={500} />
+            </div>
+            <div>
+                <h1>Recipe Name: <span className="font-bold text-2xl">{details.strMeal}</span></h1>
+                <div className="tags mt-3">
+                    <p>Ingredients List:</p>
+                    {ingredients.map((ing, idx) => (
+                        <span className="bg-blue-500 text-white px-2 py-1 rounded inline-block mr-2 mb-2" key={idx}>{ing}</span>
+                    ))}
+                </div>
+                {details.strYoutube && (
+                    <div className="mt-3">
+                        <p>Video link: </p>
+                        <a target="_blank" rel="noreferrer" className="text-blue-500" href={details.strYoutube}>How to make {details.strMeal}</a>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,16 +1,28 @@
+"use client"
+
 import React from "react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 type Props = {}
 
 const Header = (props: Props) => {
+  const pathname = usePathname()!.split("/");
+  const currentArea = pathname[2];
+  const recipeID = pathname[3];
+
   return (
-    <div className="py-5 bg-slate-300">
+    <div className="py-5 bg-slate-300 flex items-center justify-between px-2">
         <div>
             <Link href="/">
                 <h1 className="text-blue-700 font-bold text-5xl text-center">PanPal</h1>
             </Link>
         </div>
+        {pathname && currentArea && (
+          <Link className="bg-blue-500 text-white p-4 text-xs rounded font-bold" href={recipeID ? `/types/${currentArea}` : "/types"}>
+            Back to {recipeID ? `${currentArea} recipes` : "recipe types"}
+          </Link>
+        )}
     </div>
   )
 }
