c199794d147b679d8311fe571fad8935dedef9a1
Enric Carstens <enric.carstens@gmail.com>
2023-02-14 19:03:07 +0100

    generatestaticparams

--- app/types/page.tsx
+++ app/types/page.tsx
@@ -5,7 +5,7 @@ interface Meal {
     strArea: string;
 }
 
-const fetchRecipesAreas = async (): Promise<string[]> => {
+export const fetchRecipesAreas = async (): Promise<string[]> => {
     // await new Promise((res) => setTimeout(res, 5000));
     const res = await fetch("https://www.themealdb.com/api/json/v1/1/list.php?a=list");
     const response = await res.json();
